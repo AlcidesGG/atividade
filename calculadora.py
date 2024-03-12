3452ec18e79cea92e90392bfb7ec7965416f711d
AlcidesGG <162999265+AlcidesGG@users.noreply.github.com>
2024-03-12 07:50:58 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,6 @@
+numero1=float(input("digite um numero"))
+numero2=float(input("digite um numero"))
+#numero3=float(input("digite um numero"))
+#numero4=float(input("digite um numero"))
+soma= numero1 + numero2
+print("a soma é",soma)
